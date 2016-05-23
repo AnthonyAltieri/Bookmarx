484d707e0916e9e3b3bf39bb9741aaaa427267d7
Abbas Dharamsey <adharams@ucsd.edu>
2016-05-23 14:25:50 -0700

    url, empty/null checks

--- src/UtilService.js
+++ src/UtilService.js
@@ -1,11 +1,25 @@
 var UtilService = {};
 
 UtilService.checkUndefined = checkUndefined;
+UtilService.checkURL = checkURL;
+UtilService.checkString = checkString;
 
 function checkUndefined(obj) {
   if (typeof obj === 'undefined') {
     throw 'Error: ' + obj + ' is undefined';
   }
 }
+
+function checkURL(url) {
+  var urlExpression = /((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/;
+  var urlRegex = new RegExp(urlExpression);
+
+  return urlRegex.test(url);
+}
+
+function checkString(s) {
+  return ((s.trim() === "") || (s.trim() === "null") || (s.trim() === "NULL"));
+}
+
 
 module.exports = UtilService;
