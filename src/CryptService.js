df9d719bb73a2a0f66dc9bee61e95aa5217499ac
Abbas Dharamsey <adharams@ucsd.edu>
2016-05-23 20:54:22 -0700

    session

---
+++ src/CryptService.js
@@ -0,0 +1,18 @@
+var CryptService = [];
+
+var bcrypt = require('bcryptjs');
+
+CryptService.hash = hash;
+CryptService.comparePassword = comparePassword;
+
+var salt = "ourSalt";
+
+function hash(username, password) {
+  return bcrypt.hashSync(password+username+salt);
+}
+
+function comparePassword(username, password, hash) {
+  return bcrypt.compareSync(password+username+salt, hash);
+}
+
+module.export = CryptService;
